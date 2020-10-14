f9f1f27c946291e35c2563a73d1d5d263a80e1b7
Likhit Garimella <likhitgmella@gmail.com>
2020-10-15 05:09:25 +0530

    firebaseConfig init-firebase.js

---
+++ resources/js/init-firebase.js
@@ -0,0 +1,17 @@
+// Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
+  authDomain: "omnifood-likhit2304.firebaseapp.com",
+  databaseURL: "https://omnifood-likhit2304.firebaseio.com/",
+  projectId: "omnifood-likhit2304",
+  storageBucket: "omnifood-likhit2304.appspot.com",
+  messagingSenderId: "65211879809",
+  appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
+  measurementId: "G-8GSGZQ44ST"
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
