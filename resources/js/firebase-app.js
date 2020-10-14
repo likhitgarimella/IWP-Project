18bc31c7c8eae302e9fcc4c096d9e03decba0c31
Likhit Garimella <likhitgmella@gmail.com>
2020-10-15 05:03:57 +0530

    firebaseConfig

---
+++ resources/js/firebase-app.js
@@ -0,0 +1,15 @@
+// For Firebase JavaScript SDK v7.20.0 and later,
+// `measurementId` is an optional field
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
+  authDomain: "omnifood-likhit2304.firebaseapp.com",
+  databaseURL: "https://omnifood-likhit2304.firebaseio.com/",
+  projectId: "omnifood-likhit2304",
+  storageBucket: "omnifood-likhit2304.appspot.com",
+  messagingSenderId: "65211879809",
+  appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
+  measurementId: "G-8GSGZQ44ST"
+
+};
